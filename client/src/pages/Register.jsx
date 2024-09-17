3dc30b66ed93208182dccf813bf88ff9572cb388
YedhuKrishna01 <yedhukrishnagk@gmail.com>
2024-09-17 19:45:29 +0530

    initiating pages and format

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { Form } from  "antd"
+import { Form, Input } from  "antd"
+import FormItem from 'antd/es/form/FormItem';
+import "../styles/Register.css"
+
 function Register() {
 
   const onfinishHandler = (values) =>{
@@ -8,8 +11,20 @@ function Register() {
   }
   return (
     <div>
-      <div className="form-container">
-        <Form layout='vertical' onFinish={onfinishHandler}>grg</Form>
+      <div className="flex items-center justify-center h-[100vh]">
+        <Form className='p-10 border-[1px] rounded-lg' layout='vertical' onFinish={onfinishHandler}>
+          <h1>Register Form</h1>
+          <FormItem label="Name" name="name">
+            <Input type='text'required/>
+          </FormItem>
+          <FormItem label="Email" name="email">
+            <Input type='email'required/>
+          </FormItem>
+          <FormItem label="Password" name="password">
+            <Input type='password'required/>
+          </FormItem>
+          <button type='submit'>Register</button>
+        </Form>
       </div>
     </div>
   )
